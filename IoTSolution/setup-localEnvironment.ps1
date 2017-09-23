e6d904789e79e15efd31602a2c0bf0f9dff7005f
To To <toto_san@live.com>
2017-09-23 20:17:36 +0200

    adjusted paths for env-setup

--- IoTSolution/setup-localEnvironment.ps1
+++ IoTSolution/setup-localEnvironment.ps1
@@ -1,7 +1,7 @@
-$includeSetup = Get-Content .\.vscode\c_cpp_properties.json
+$includeSetup = Get-Content ..\.vscode\c_cpp_properties.json
 $includeSetup = $includeSetup -replace "__path__", $env:USERPROFILE
 $includeSetup = $includeSetup -replace '\\','/'
-Set-Content -Path .\.vscode\c_cpp_properties.json -Value $includeSetup
+Set-Content -Path ..\.vscode\c_cpp_properties.json -Value $includeSetup
 
 .\Deploy.ps1
 .\Deployments\DeployBiSettings.ps1 -fromPath .
