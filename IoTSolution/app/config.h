7700938f99169b9c4432b833a5dea835500ffb60
Thomas Tomow <toto_san@live.com>
2017-10-02 20:20:20 +0200

    configuration changes

--- IoTSolution/app/config.h
+++ IoTSolution/app/config.h
@@ -1,3 +1,3 @@
 static const char* connectionString = "[IoT device connection string]";
-static const char* ssid="";
-static const char* pass="";
+static const char* ssid="MyWlan";
+static const char* pass="x24_$5N9?";
